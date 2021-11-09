b5af4f78cadb7a404b274eb62166e89de34b4027
Kim Kroener <kim-kroener@t-online.de>
2021-11-09 18:15:40 +0100

    implemented field variable

--- src/storage/field_variable.cpp
+++ src/storage/field_variable.cpp
@@ -1,51 +1,47 @@
 #pragma once
 
 #include <memory>
+#include "storage/field_variable.h"
 #include "storage/array2D.h"
 
 FieldVariable::FieldVariable(std::array<int,2> size, std::array<double,2> origin, std::array<double,2> meshWidth) :
-size_=size,
-origin_=origin,
-meshWidth_=meshWidth {
-
+Array2D(size), 
+origin_(origin), 
+meshWidth_(meshWidth)
+{
 }
 
 
 double FieldVariable::interpolateAt(double x, double y) const 
 {    
-const double hx = meshWidth_[0]; // mesh width in x dir. 
-const double hy = meshWidth_[1]; // mesh width in y dir. 
+    const double dx = meshWidth_[0]; // mesh width in x dir. 
+    const double dy = meshWidth_[1]; // mesh width in y dir. 
 
-// indicies of (cell (i,j) in which the point (x,y) lies
-const double iLeftEdge  = floor(x / hx); 
-const double iRightEdge = ceil(x / hx); 
+    // indicies of (cell (i,j) in which the point (x,y) lies
+    const int iLeftEdge  = (int) floor(x - origin_[0] / dx); 
+    const int jLowerEdge = (int) floor(y - origin_[1] / dy); 
 
-const double jLowerEdge = floor(y / hy); 
-const double jUpperEdge = ceil(y / hy); 
+    // get values at corner points
+    const double f_lowerLeft  = Array2D::operator()(iLeftEdge,     jLowerEdge);
+    const double f_upperLeft  = Array2D::operator()(iLeftEdge,     jLowerEdge + 1); 
+    const double f_lowerRight = Array2D::operator()(iLeftEdge + 1, jLowerEdge); 
+    const double f_upperRight = Array2D::operator()(iLeftEdge + 1, jLowerEdge + 1);
 
-// edge points of cell i,j
-const double[2] edgeLowerLeft  = {iLeftEdge, jLowerEdge};
-const double[2] edgeUpperLeft  = {iLeftEdge, jUpperEdge};
+    // relative position of x and y in the cell
+    // one cell: |<-xr1-> x <-xr2->|
+    //           |<--    dx      ->|
 
-const double[2] edgeLowerRight = {iRightEdge, jLowerEdge};
-const double[2] edgeUpperRight = {iRightEdge, jUpperEdge};
+    const double xr1 = x % dx;   // relative position of x from left edge
+    const double yr1 = y % dy;   // relative poistion of y from lower edge
+    const double xr2 = dx - xr1; // distance right_edge - x
+    const double yr2 = dy - yr1; // distance upper edge - y
 
-// values at the points
-const double edgeLowerLeft_value = data_[(int)((edgeLowerLeft[0]-1) * (size_[0])) + edgeLowerLeft[1])];
-const double edgeUpperLeft_value = data_[(int)((edgeUpperLeft[0]-1) * (size_[0])) + edgeUpperLeft[1])];
-const double edgeLowerRight_value = data_[(int)((edgeLowerRight[0]-1) * (size_[0])) + edgeLowerRight[1])];
-const double edgeUpperRight_value = data_[(int)((edgeUpperRight[0]-1) * (size_[0])) + edgeUpperRight[1])];
+    // bilinear interpolation
+    f_intp = (f_lowerLeft * xr2 * yr2 
+        + f_lowerRight * xr1 * yr2
+        + f_upperLeft  * xr2 * yr1
+        + f_upperRight * xr1 * xr1) / (dx * dy);
 
-// relative position of x and y in the cell
-const double dx = x % hx; // relative position of x from left edge
-const double dy = x % hy; // relative poistion of y from lower edge
-
-// bilinear interpolation
-const double i_intp_LowerEdge = (iLeftEdge - x )/(iRightEdge - iLeftEdge) * edgeLowerLeft_value + (x - iLeftEdge)/(iRightEdge - iLeftEdge) * edgeLowerRight_value;
-const double i_intp_UpperEdge = (iLeftEdge - x )/(iRightEdge - iLeftEdge) * edgeUpperLeft_value + (x - iLeftEdge)/(iRightEdge - iLeftEdge) * edgeUpperRight_value;
-
-const double j_intp_LeftEdge  = 
-
-return ;
+return f_intp;
 
 }
