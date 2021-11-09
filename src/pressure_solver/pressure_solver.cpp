1fb78f01b49bc032d436cbc2d442170811b35bde
kimkroener <78850600+kimkroener@users.noreply.github.com>
2021-11-09 21:25:46 +0100

    added comments

--- src/pressure_solver/pressure_solver.cpp
+++ src/pressure_solver/pressure_solver.cpp
@@ -17,7 +17,9 @@ PressureSolver::PressureSolver(std::shared_ptr<Discretization> discretization, d
       //p_i,-1 = p_i,0. p_i,n+1 = p_i,n.
       for ( int i = discretization_->pIBegin() +1; i < discretization_->pIEnd() -1; i++)
       {
-        discretization_->p(i, discretization_->pJBegin()) =  discretization_->p(i, discretization_->pJBegin() +1);
+        // bottom
+        discretization_->p(i, discretization_->pJBegin())  = discretization_->p(i, discretization_->pJBegin() +1);
+        // top
         discretization_->p(i, discretization_->pJEnd() -1) = discretization_->p(i, discretization_->pJEnd() -2);
       }
 
@@ -25,7 +27,9 @@ PressureSolver::PressureSolver(std::shared_ptr<Discretization> discretization, d
        // p_-1,j = p_0,j. p_n+1,j = p_n,j.
       for ( int j = discretization_->pJBegin(); j < discretization_->pJEnd(); j++)
       {
+        // left
         discretization_->p(discretization_->pIBegin(),j) =  discretization_->p(discretization_->pIBegin()+1,j);
+        // right
         discretization_->p(discretization_->pIEnd() -1,j) =  discretization_->p(discretization_->pIEnd() -2,j);
       }
 
