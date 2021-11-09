ecbdcb650332bbd4fb95d617ba0f736945ccede7
Janis Wissinger <janis.wissinger@web.de>
2021-11-09 15:54:17 +0100

    bug fixes

--- src/pressure_solver/gauss_seidel.cpp
+++ src/pressure_solver/gauss_seidel.cpp
@@ -27,15 +27,16 @@ void GaussSeidel::solve()
 
     // iterate through grid 
     
-    while( iteration < maximumNumberOfIterations_ && res > dx2 * dy2 * epsilon_)
+     while( iteration < maximumNumberOfIterations_ && res > dx2 * dy2 * epsilon_)
     {
         for ( int j = discretization_->pJBegin() +1 ; j < discretization_->pJEnd() -1; j++)
         { 
             for ( int i = discretization_->pIBegin() +1; i < discretization_->pIEnd() -1; i++)
             {
-            discretization_->p(i, j) = (dx2 * dy2) / ( 2 * dx2 * dy2) *(pxx + pyy - discretization_->rhs(i, j));
-            pxx = (discretization_->p(i+1, j) - discretization_->p(i-1, j)) / (2 *dx2);
-            pyy = (discretization_->p(i, j+1) - discretization_->p(i, j-1)) / (2 * dy2);
+            pxx = (discretization_->p(i+1, j) + discretization_->p(i-1, j)) / (dx2);
+            pyy = (discretization_->p(i, j+1) + discretization_->p(i, j-1)) / (dy2);    
+            discretization_->p(i, j) = (dx2 * dy2) / ( 2 * (dx2 + dy2)) *(pxx + pyy - discretization_->rhs(i, j));
+            
 
             // calculate residual 
             resij = discretization_->rhs(i, j) - pxx - pyy;   
