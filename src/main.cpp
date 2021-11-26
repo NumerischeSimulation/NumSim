059b139953ccbcbe54bb6e76b7d47d12a4ee802a
kimkroener <78850600+kimkroener@users.noreply.github.com>
2021-11-26 13:20:27 +0100

    added a semicolon

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
   std::cout << std::endl;
   
   // // start simulating
-  computation.runSimulation()
+  computation.runSimulation();
 
   // test some stuff
   //computation.runTest();
