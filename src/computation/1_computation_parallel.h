8580c0d0b141e0f8d029a3db7c926664603c24a6
Kim Kroener <kim-kroener@t-online.de>
2021-11-26 16:41:36 +0100

    updated parallel computation of dt_

--- src/computation/1_computation_parallel.h
+++ src/computation/1_computation_parallel.h
@@ -14,17 +14,17 @@ class ComputationParallel: public Computation
 {
 public:    
     //! initialize the computation object, parse the settings from file that is given as the only command line argument
-    void initialize(int argc, char *argv[]);
+    void initialize(int argc, char *argv[]); // overwrite
 
     //! run the whole simulation until t_end 
-    void runSimulation();
+    void runSimulation(); // overwrite
 
 protected:
     //! compute the time step width dt from maximum velocities 
-    void computeTimeStepWidth();
+    void computeTimeStepWidthParallel(double currentTime);
 
     //!  set boundary values of u and v to correct values
-    void applyBoundaryValues();
+    void applyBoundaryValues();  // overwrite of method by Computation
     void applyBoundaryValuesLeft();
     void applyBoundaryValuesRight();
     void applyBoundaryValuesTop();
