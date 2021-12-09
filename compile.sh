f8c7c83f740c6c539a05dfd8cacd31496399b38c
MagnusOstertag <MagnusOstertag@users.noreply.github.com>
2021-12-09 16:55:55 +0000

    Load modules for remote deployment

--- compile.sh
+++ compile.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# load modules
+module use /usr/local.nfs/sgs/modulefiles
+module load gcc/10.2
+module load openmpi/3.1.6-gcc-10.2
+module load vtk/9.0.1
+module load cmake/3.18.2
+
 # clean directory
 rm -rf build/
 
